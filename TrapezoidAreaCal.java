7fb781453c7aaef13908f79ae527b4990a2a113d
mdivyaswarupa2004 <divyaswarupa@everest.engineering>
2025-05-31 20:24:35 +0530

    fix: resolve conflict between addition and division logic during rebase

--- TrapezoidAreaCal.java
+++ TrapezoidAreaCal.java
@@ -4,10 +4,17 @@ public class TrapezoidAreaCal {
         return a + b;
     }
 
+    public static int divisionByTwo(int number) {
+        return number / 2;
+    }
+
     public static void main(String[] args) {
         System.out.println("Trapezoid Area Calculator");
 
         int result = add(4, 6);
         System.out.println("4 + 6 = " + result);
+
+        result = divisionByTwo(result);
+        System.out.println("10 / 2 = " + result);
     }
 }
